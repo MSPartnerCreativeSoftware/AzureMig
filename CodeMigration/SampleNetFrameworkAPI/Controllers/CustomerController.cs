e7ebb7cf2ef183698d34c8f416415b506fb28bbd
Dumindu De Silva <Dumindu@CreativeSoftware.com>
2021-07-29 18:20:58 +0530

    added customercontroller .netcore version in comments

--- CodeMigration/SampleNetFrameworkAPI/Controllers/CustomerController.cs
+++ CodeMigration/SampleNetFrameworkAPI/Controllers/CustomerController.cs
@@ -4,37 +4,42 @@ using System.Web.Http;
 
 namespace SampleNetFrameworkAPI.Controllers
 {
+    //.NET5 VERSION
+    //[ApiController]
+    //public class CustomerController : ControllerBase
     public class CustomerController : ApiController
     {
         private List<Customer> _customers = new List<Customer> { new Customer { Id = 1, Name = "John" }, new Customer { Id = 2, Name = "Doe" } };
 
-        // GET api/values
+
         public IHttpActionResult GetCustomers()
         {
             return Json(_customers);
         }
 
-        // GET api/values/5
+
         public IHttpActionResult GetCustomerById(int id)
         {
 
             return Json(_customers.FindAll(customer => customer.Id == id));
         }
 
-        //// POST api/values
-        //public void Post([FromBody] Customer value)
+        ////.NET5 VERSION
+        //[HttpGet]
+        //[Route("api/Customers")]
+        //// GET api/values
+        //public IActionResult GetCustomers()
         //{
+        //    return Ok(_customers.ToArray());
         //}
 
-        //// PUT api/values/5
-        //public void Put(int id, [FromBody] string value)
+        //[HttpGet]
+        //[Route("api/Customer/{id}")]
+        //// GET api/values/5
+        //public IActionResult GetCustomerById(int id)
         //{
-            
-        //}
 
-        //// DELETE api/values/5
-        //public void Delete(int id)
-        //{
+        //    return Ok(_customers.FindAll(customer => customer.Id == id).ToArray());
         //}
     }
 }
